ee427ad42d74fe7da3a5976e09c9af3fa65f4153
luiscarabajal <luispires.m@gmail.com>
2020-06-22 01:56:40 -0300

    Primeira aplicação em ASP Net MVC

---
+++ PrimeiroProjeto/Models/Fabricante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimeiroProjeto.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+        public String Nome { get; set; }
+
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ PrimeiroProjeto/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrimeiroProjeto.Models
+{
+    public class Produto
+    {
+        public long? ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public long? CategoriaId { get; set; }//chave estrangeira e pode ser null '?'
+        public long? FabricanteId { get; set; } //idem
+        public Categoria Categoria { get; set; }
+        public Fabricante Fabricante { get; set; }
+    }
+}
